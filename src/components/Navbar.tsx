63bcd5e0bbec0fadc927d583ea85e37c9b7cd27b
Denis Leme <denis_leme@icloud.com>
2023-09-02 18:34:17 -0300

    Finalizado Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = () =>{
     return(
-        <nav id="Navbar">
-        <div className="bg-green-500 h-14 flex items-center justify-between border-spacing-y-4">
-        <a className="text-yellow-400 text-base  pl-7 mr-8" href="#">Home</a>
+        <nav id="Navbar" className="max-w-full ">
+        <div className="bg-green-500 h-14 flex items-center justify-center">
+        <a className="text-yellow-400 text-base  pl-7 " href="#">Home</a>
         <a className="text-yellow-400 text-base  pl-7" href="#">Gerador de bets</a>
         <a className="text-yellow-400 text-base  pl-7 mr-11" href="#">Noticias</a>
         </div>
